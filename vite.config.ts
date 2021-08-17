67facfcf88aa50aad4c6f695c9cd6c0623332e97
Mattias Bonte <mb@guideline.be>
2021-08-17 16:03:21 +0200

    Deleted PWA until further notice...

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'safari-pinned-tab.svg'],
-      manifest: {
-        name: 'Bloom With Tea Deck',
-        short_name: 'BWTD',
-        description: 'Sit with tea. Draw a card. Enjoy.',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'android-chrome-192x192.png?v=1',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'android-chrome-512x512.png?v=1',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'android-chrome-512x512.png?v=1',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [reactRefresh()]
 })
